84ed815dece5804dd9049d9ab0c1fb38d9d5b5ba
Winnie Wen <wiwen@berkeley.edu>
2017-02-01 10:42:53 -0800

    Queue implementation done using linkedlist

--- queue.py
+++ queue.py
@@ -1,11 +1,12 @@
 #!python
+from linkedlist import LinkedList
 
 class Queue(object):
 
     def __init__(self, iterable=None):
         """Initialize this queue and enqueue the given items, if any"""
-        # TODO: initialize instance variables
-        pass
+        # initialize instance variables
+        self.queue = LinkedList()
         if iterable:
             for item in iterable:
                 self.enqueue(item)
@@ -16,27 +17,33 @@ class Queue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise"""
-        # TODO: check if empty
-        pass
+        if self.queue.count == 0:
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this queue"""
-        # TODO: count number of items
-        pass
+        return self.queue.count
 
     def peek(self):
         """Return the next item in this queue without removing it,
         or None if this queue is empty"""
-        # TODO: return next item, if any
-        pass
+        if self.is_empty():
+            return None
+        return self.queue.head.data
 
     def enqueue(self, item):
         """Enqueue the given item into this queue"""
-        # TODO: enqueue given item
-        pass
+        # enqueue given item
+        self.queue.append(item)
+
 
     def dequeue(self):
         """Return the next item and remove it from this queue,
         or raise ValueError if this queue is empty"""
-        # TODO: dequeue next item, if any
-        pass
+        if self.is_empty():
+            raise ValueError
+        else:
+            first_item = self.queue.head.data
+            self.queue.delete(first_item)
+            return first_item

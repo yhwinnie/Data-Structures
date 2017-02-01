dad0374b65804f19e27b90a256cfed1ad4629ed6
Winnie Wen <wiwen@berkeley.edu>
2017-02-01 10:48:05 -0800

    Stack implementation done using dynamic array

--- stack.py
+++ stack.py
@@ -6,7 +6,8 @@ class Stack(object):
     def __init__(self, iterable=None):
         """Initialize this stack and push the given items, if any"""
         # initialize instance variables
-        self.stack = LinkedList()
+        # self.stack = LinkedList()
+        self.stack = []
         if iterable:
             for item in iterable:
                 self.push(item)
@@ -17,20 +18,35 @@ class Stack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise"""
-        if self.stack.count == 0:
+        # LinkedList
+        # if self.stack.count == 0:
+        #     return True
+        # return False
+
+        # Array
+        if len(self.stack) == 0:
             return True
         return False
 
     def length(self):
         """Return the number of items in this stack"""
-        return self.stack.count
+        # LinkedList
+        # return self.stack.count
+
+        #Array
+        return len(self.stack)
 
     def peek(self):
         """Return the top item on this stack without removing it,
         or None if this stack is empty"""
         if self.is_empty():
             return None
-        return self.stack.tail.data
+        # LinkedList
+        # return self.stack.tail.data
+
+        # Array
+        return self.stack[self.length() - 1]
+
 
     def push(self, item):
         """Push the given item onto this stack"""
@@ -39,9 +55,17 @@ class Stack(object):
     def pop(self):
         """Return the top item and remove it from this stack,
         or raise ValueError if this stack is empty"""
-        if self.stack.count == 0:
+        # Linked List
+        # if self.is_empty():
+        #     raise ValueError
+        # else:
+        #     top_item = self.stack.tail.data
+        #     self.stack.delete(top_item)
+        #     return top_item
+
+        # Array
+        if self.is_empty():
             raise ValueError
         else:
-            top_item = self.stack.tail.data
-            self.stack.delete(top_item)
-            return top_item
+            top_item = self.stack[self.length() - 1]
+            return self.stack.pop()
